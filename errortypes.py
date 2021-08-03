0764b94f1886082be0430faef92520473b0ff809
farhana <86741342+farhasony@users.noreply.github.com>
2021-08-03 18:09:13 +0530

    Add files via upload

---
+++ errortypes.py
@@ -0,0 +1,17 @@
+dict={"1":"1","2":"3","3":"23"}
+x=dict["2"] #if we write 4 we get key error
+#x=dict.get('4')
+print(x)
+list=[1,2,3]
+print(list[0])
+#print(list[4]) index error
+xyz=800
+print(xyz)
+#print(xy) name error
+#1/0 zero division error
+#ord(1) type error
+x=ord("a")
+print(x)
+x=chr(97)
+print(x)
+
